9233dde3eb3384bdf9bf44b7314b0b6288997619
Ayrton Ciancimino <ajciancimino@usf.edu>
2020-02-05 18:59:51 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 25;
+            int[] countdown = new int[size];
+
+            // Iterate through the array using a For Loop
+            for (int i = 1; i <= size; i++)
+            {
+
+                Console.WriteLine("Element value ="+ " " +i);
+
+            }
+
+            
+        }
+    }
+}
